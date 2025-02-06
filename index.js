49f2831da902a628a9d54f72ca9e9a262b005620
smartsusee <susee37432@gmail.com>
2025-02-06 17:22:24 +0530

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const ErrorHandling = require("./errorHandling")
 mongoose.connect("mongodb://127.0.0.1:27017/Employee",{
    useNewUrlParser: true,
    useUnifiedTopology: true,
-   bufferTimeoutMS: 30000, // Increase timeout to 30 seconds
+   serverSelectionTimeoutMS: 5000,
  }).then(() => {
    console.log("db is  connected");
 
